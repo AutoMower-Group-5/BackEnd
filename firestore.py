8e51f810565c7ccadaa575cf051b699e57170b98
Andrey <andrey.arronet@hotmail.se>
2023-03-31 12:03:01 +0200

    did some tests using python and flask and conected to firebase

---
+++ firestore.py
@@ -0,0 +1,25 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use a service account.
+cred = credentials.Certificate("serviceAccountKey.json")
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+def writeData():
+    doc_ref = db.collection(u'Robot').document(u'Position')
+    doc_ref.set({
+        u'X': u'5',
+        u'Y': u'10'
+    })
+
+def readData():
+    users_ref = db.collection(u'Robot')
+    docs = users_ref.stream()
+
+    # for doc in docs:
+    #     print(f'{doc.id} => {doc.to_dict()}')
+    return docs
+
+readData()
